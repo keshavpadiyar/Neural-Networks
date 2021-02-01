8344295cb78c9d6e1967c6e25af76fef8e7faf25
keshavpadiyar <keshavpadiyar.07@gmail.com>
2021-02-01 23:13:00 +0100

    Updated KNN
    Some minor modifications in knn.m file

--- A1_Supervised/kNN/kNN.m
+++ A1_Supervised/kNN/kNN.m
@@ -15,20 +15,21 @@ function [ LPred ] = kNN(X, k, XTrain, LTrain)
     y = LTrain;
     [d_nrow, d_ncol] = size(data);
     [x_nrow, x_ncol] = size(x);
-    dist = [];
+    dist = zeros(size(data,1),1);
     LPred  = zeros(size(X,1),1);
-    final_class = [];
+    final_class = zeros(size(data,1),1);
 
 % Row wise looping to calculate Eucledian Distance between data points
 % Summing the distances obtained from all variables (columns) for a
 % particular row
     for x_index = 1:x_nrow
          for d_index = 1:d_nrow
-            dist(d_index) = sum(sqrt((data(d_index,1:2)-x(x_index,:)).^2)); % Eucledian Distance
+            dist(d_index) = sum(sqrt((data(d_index,:)-x(x_index,:)).^2)); % Eucledian Distance
+            final_class(d_index)=y(d_index);
          end
         [d,i] = sort(dist); % sort the distances in ascending order
-        i = i(1:k); % select the k nearest distance 
-        [c,v] = groupcounts(y(i)); % get the element wise count (similar to table operation in R)
+        i = i(1:k); % select the k nearest distance
+        [c,v] = groupcounts(final_class(i)); % get the element wise count (similar to table operation in R)
         v = v(find(c==max(c),1)); % Majority Voting (if many classes distances comes same for a data point then select the first class)
         LPred(x_index) = v;
     end
